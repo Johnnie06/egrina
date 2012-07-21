a034e63681851a10f59d4c3b997052ddde85ed91
Park Jiyeon <parkjy@ais-info.co.jp>
2012-07-21 16:22:41 +0900

    안녕

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12. 7. 21
+ * Time: 오후 4:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+
+}
